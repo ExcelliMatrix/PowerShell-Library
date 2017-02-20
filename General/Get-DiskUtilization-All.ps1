e917f89ba892de02162ac32d57b5ff74ffffa116
Leland R. Johnson <Leland.Johnson@ExcelliMatrix.local>
2017-02-20 11:25:26 -0700

    Updated the scripts header to reflect MIT license.

--- General/Get-DiskUtilization-All.ps1
+++ General/Get-DiskUtilization-All.ps1
@@ -2,6 +2,8 @@
 #                                                                  #
 # Copyright (c) 2016-2017 ExcelliMatrix, Inc. All Rights Reserved. #
 #                                                                  #
+# Provided under MIT license                                       #
+#                                                                  #
 ####################################################################
 
 param
